a03a1e6dfa3523e5e05f84f54597651f27b3a087
VictorCrespim <127981947+VictorCrespim@users.noreply.github.com>
2024-05-10 20:46:02 -0300

    Add files via upload

--- js/javascript-topo.js
+++ js/javascript-topo.js
@@ -18,23 +18,19 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var botaoAbrir = document.getElementById('abrir-nav');
-    var meuDropdown = document.getElementById('nav-dropdown');
-  
-    botaoAbrir.addEventListener('click', function() {
-      if (meuDropdown.style.display === 'block') {
-        meuDropdown.style.display = 'none';
-      } else {
-        meuDropdown.style.display = 'block';
-      }
-    });
-  
-    // Fechar dropdown quando clicar fora dele
-    document.addEventListener('click', function(event) {
-      if (event.target !== botaoAbrir && event.target !== meuDropdown) {
-        meuDropdown.style.display = 'none';
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
 
-      }
-    });
-  });
+btnMenu.addEventListener('click', ()=>{
+  menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+  menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+  menu.classList.remove('abrir-menu')
+})
+
